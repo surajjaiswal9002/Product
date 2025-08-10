64541fd559c8a2785c82675d7f72b5868f71c280
surajjaiswal9002 <surajjaiswal9002@gmail.com>
2025-08-10 09:09:56 +0530

    10 Aug

---
+++ src/main/java/com/Target/Product/DSA/BuyAndSellStock.java
@@ -0,0 +1,27 @@
+package com.Target.Product.DSA;
+
+public class BuyAndSellStock {
+	public static void main(String[] args) {
+		
+		int[] prices = {7, 1, 5, 3, 6, 4};
+		
+		maxProfit(prices);
+	}
+	// Tc - O(n) and sc - O(1)
+	public static void maxProfit(int[] prices) {
+		int min = Integer.MAX_VALUE;
+		int maxProfit = 0;
+		int n = prices.length;
+		
+		for(int i = 0; i < n; i++) {
+			if(prices[i] < min) {
+				min = prices[i];
+			}else {
+				maxProfit = Math.max(maxProfit, prices[i] - min);
+			}
+		}
+		
+		System.out.println(" Buy and Sell Stock - Max Profit : "+maxProfit);
+		
+	}
+}
---
+++ src/main/java/com/Target/Product/DSA/ChocolateDistribution.java
@@ -0,0 +1,48 @@
+package com.Target.Product.DSA;
+
+import java.util.Arrays;
+
+public class ChocolateDistribution {
+	public static void main(String[] args) {
+		
+		int[] students = {4, 1, 2, 3};
+		
+		isMinChocolateDist(students);
+	}
+	
+	public static void isMinChocolateDist(int[] ranks) {
+		int n = ranks.length;
+		int[] chocolatesDist = new int[n];
+		
+		
+		//Step 1-  given each student 1 chocolate.
+		for(int i = 0; i < n; i++) {
+			chocolatesDist[i] = 1;
+		}
+		
+		// Step 2  give min chocolate from left to right
+		for(int i = 1; i < n; i++) {
+			if(ranks[i] < ranks[i - 1]) {
+				chocolatesDist[i] = chocolatesDist[i - 1] + 1;
+			}
+		}
+		
+		// step 3 given chocolate from right to left 
+		for(int i = n - 2; i >= 0; i--) {
+			if(ranks[i] < ranks[i + 1]) {
+				chocolatesDist[i] = Math.max(chocolatesDist[i], chocolatesDist[i + 1] + 1);
+			}
+		}
+		
+		
+		// step 4: total chocolate
+		int totalChocolateDist = 0;
+		for(int c : chocolatesDist) {
+			totalChocolateDist += c;
+		}
+		
+		
+		System.out.println(" Student chocolate distributed : "+Arrays.toString(chocolatesDist));
+		System.out.println(" Total Chocolate Distribution : "+totalChocolateDist);
+	}
+}
