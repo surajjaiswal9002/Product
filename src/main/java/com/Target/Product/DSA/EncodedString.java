a07565bb653712e78917615476c6b5a66563b4a0
surajkumarjaiswal <surajjaiswal9002@gmail.com>
2025-08-11 13:35:59 +0530

    EncodedString

---
+++ src/main/java/com/Target/Product/DSA/EncodedString.java
@@ -0,0 +1,32 @@
+package com.Target.Product.DSA;
+
+public class EncodedString {
+	public static void main(String[] args) {
+		String s = "ABABCABABCE";
+		
+		encodedString(s);
+	}
+	
+	public static void encodedString(String s) {
+		int n = s.length();
+		String[] encoded = new String[n + 1];
+		
+		encoded[0] = "";
+		
+		for(int i = 1; i <= n; i++) {
+			encoded[i] = encoded[i - 1] + s.charAt(i - 1);
+			for(int j = 1; j <= i / 2; j++) {
+				if(s.substring(i - 2 * j, i - j).equals(s.substring(i - j, i))) {
+					String tempEncoded = encoded[i - j]+"*";
+					if(tempEncoded.length() < encoded[i].length()) {
+						encoded[i]= tempEncoded;
+						
+					}
+				}
+			}
+		}
+		
+		
+		System.out.println(" encoded String "+encoded[n]);
+	}
+}
